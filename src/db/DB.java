4c72581d43023f4e3dfe9e17a7dd07cb766cdf6a
Luan <luan.donato10@gmail.com>
2024-10-04 18:21:16 -0300

    Department Dao e Seller Dao

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,6 +1,7 @@
 package db;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,4 +14,36 @@ public class DB {
 	
 	public static Connection conn = null;
 	
+	public static Connection getConnection(){
+		if(conn != null) {
+			try {
+				Properties props = loadProperties();
+		
+				String DBurl = props.getProperty("DBurl");
+			
+				conn = DriverManager.getConnection(DBurl, props);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return conn;
+	}
+	
+	private static Properties loadProperties() {
+		Properties props = new Properties();
+		
+		try(FileInputStream fs = new FileInputStream("DB.properties")){
+			
+			props.load(fs);
+			return props;
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return props;
+	}
 }
